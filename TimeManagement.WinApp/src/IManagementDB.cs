4c3e3b5e0ecbf1f2bb1ddcf39585f62893b7b1d2
unknown <Mario@.(none)>
2009-05-11 20:41:14 +0800

    modified files.  Added sqlite instead of using csv.
    Signed-off-by: webiest <webiest@gmail.com>

---
+++ TimeManagement.WinApp/src/IManagementDB.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeManagement.WinApp
+{
+    public interface IManagementDB
+    {
+        void InsertRecordEntry(RecordEntry item);        
+    }
+}
